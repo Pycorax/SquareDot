8c52203b5169a03f67af02d58b4b78bdaca9f230
pycorax <msntkw@msn.com>
2015-08-15 12:07:21 +0800

    Added checking a Maps folder for map files

---
+++ Game Jam/Assets/Scripts/LevelController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class LevelController : MonoBehaviour
+{
+    // Levels
+    [Tooltip("The level to start with")]
+    public int startLevel;
+    [Tooltip("Directory of the Map Folder")]
+    public string mapFolderDirectory;
+    private int level = 0;
+    private List<Map> maps;
+
+    // Use this for initialization
+    void Awake()
+    {
+        level = startLevel;
+        maps = LoadMapList(mapFolderDirectory);
+    }
+
+    void Start () 
+    {
+	    // TODO: Load Level from File
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    // TODO: If winning condition, then go to next
+	}
+
+    List<Map> LoadMapList(string folderDir)
+    {
+        List<Map> mapList = new List<Map>();
+
+        // Find the Map Files
+        DirectoryInfo d = new DirectoryInfo(folderDir);
+        FileInfo[] filelList = d.GetFiles(Map.MAP_FILE_EXTENSION);
+
+        // Load all the Maps
+        foreach (FileInfo file in filelList)
+        {
+            Debug.Log(file.Name);
+            // Load the map
+            //Map map = Map.LoadMap(file.Name);
+            // Add the map into the list
+           // mapList.Add(map);
+        }
+
+        return mapList;
+    }
+}
---
+++ Game Jam/Assets/Scripts/Map.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Map : MonoBehaviour 
+{
+    // TODO: Add the variable that holds the Map
+    // TODO: Add a List of Enemies
+    public const string MAP_FILE_EXTENSION = "*.map";
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    public static Map LoadMap(string mapFilePath)
+    {
+        throw new NotImplementedException();
+
+        return new Map();
+    }
+}
