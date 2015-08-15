b7c88ffe4550482c66a6857abfb5cc7533c28b9a
blazetrinity <blaze-trinity@hotmail.sg>
2015-08-15 17:55:13 +0800

    AI Movement Done

---
+++ Game Jam/Assets/Scripts/EnemyAI/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : Character {
+
+	public Vector2 FacingNormal = new Vector2(1 ,0);
+	public const float ChangeFacingMaxTimer = 2.5f;
+	public float ChangeFacingTimer = ChangeFacingMaxTimer;
+	public bool ChangeFacing = false;
+
+	void Awake () {
+		base.CharacterAwake ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		CharacterRigidBody.velocity = FacingNormal * MoveAccel;
+		base.CharacterUpdate ();
+
+		ChangeFacingTimer -= Time.deltaTime;
+		if (ChangeFacingTimer <= 0) {
+			ChangeFacing = true;
+			ChangeFacingTimer = ChangeFacingMaxTimer;
+		}
+
+		if (ChangeFacing) {
+			FacingNormal = -FacingNormal;
+			ChangeFacing = false;
+		}
+	}
+}
