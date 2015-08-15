e079bf2bf98bedd78dbad7cf9765ab323f9fe6d9
blazetrinity <blaze-trinity@hotmail.sg>
2015-08-15 14:45:01 +0800

    Character Move Right Move Left

---
+++ Game Jam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public Character Character;
+	public const int FORCE = 50;
+	public const float MAXSPEED = 25.0f;
+
+	void Awake() {
+		Character = GetComponent<Character> ();
+
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Move Up
+		if (Input.GetKey (KeyCode.W)) {
+
+		}
+		//Move Down
+		else if (Input.GetKey (KeyCode.S)) {
+
+		}
+		//Move Right
+		if (Input.GetKey (KeyCode.D)) {
+			Character.CharacterRigidBody.velocity += (new Vector2(FORCE, 0) * Time.deltaTime);
+
+			/*if(Character.CharacterRigidBody.velocity.x > MAXSPEED)
+			{
+				Character.CharacterRigidBody.velocity = new Vector2(MAXSPEED, Character.CharacterRigidBody.velocity.y);
+			}*/
+		}
+		//Move Left
+		else if (Input.GetKey (KeyCode.A)) {
+			Character.CharacterRigidBody.velocity += (new Vector2(-FORCE, 0) * Time.deltaTime);
+			/*if(Character.CharacterRigidBody.velocity.x < -MAXSPEED)
+			{
+				Character.CharacterRigidBody.velocity = new Vector2(-MAXSPEED, Character.CharacterRigidBody.velocity.y);
+			}*/
+		}
+	}
+}
